f992a1a3afd007f090bcecfa02f635eea818c568
Stefan Höneise <stefan.hoeneise@plan-b-gmbh.com>
2019-08-06 14:12:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+
+namespace TestFeatureFlags
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var settings = config.Build();
+                    config.AddAzureAppConfiguration(options => {
+                        options.Connect(settings["ConnectionStrings:AppConfig"])
+                               .UseFeatureFlags();
+                    });
+                })
+                .UseStartup<Startup>();
+    }
+}
